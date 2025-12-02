baed264dc9e3eee57e3ad150270c8c6a7fc095c0
mk-hmoud <mhmd.hmoud1@outlook.com>
2025-12-02 13:33:58 +0200

    Added Pino as the server's logger.

---
+++ .changeset/five-zoos-go.md
@@ -0,0 +1,5 @@
+---
+"server": minor
+---
+
+Added Pino for logging facility.
--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
